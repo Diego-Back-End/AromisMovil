416066eaf3068444f2723b73cc67cb833f45b1e7
Diego-Back-End <escobedoambrosettidiego@gmail.com>
2025-10-23 10:38:54 -0300

    se agregaron las confirmaciones screen ,kt

--- app/src/main/java/com/example/aromismovil/view/ConfirmacionScreen.kt
+++ app/src/main/java/com/example/aromismovil/view/ConfirmacionScreen.kt
@@ -1,9 +1,6 @@
 package com.example.aromismovil.view
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -11,13 +8,12 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import com.example.aromismovil.viewmodel.UsuarioViewModel
 
 @Composable
-fun ConfirmacionScreen(navController: NavHostController) {
+fun ConfirmacionScreen(navController: NavHostController, usuarioViewModel: UsuarioViewModel) {
     Column(
-        Modifier
-            .fillMaxSize()
-            .padding(16.dp),
+        Modifier.fillMaxSize().padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
