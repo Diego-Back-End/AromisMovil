7d406f8c3dac7f6cf8757fcdd87e463556117ae9
Diego-Back-End <escobedoambrosettidiego@gmail.com>
2025-10-23 11:08:54 -0300

    se elimino el url imagen y se cambio por imagen res drawable

--- app/src/main/java/com/example/aromismovil/model/Producto.kt
+++ app/src/main/java/com/example/aromismovil/model/Producto.kt
@@ -1,12 +1,14 @@
 package com.example.aromismovil.model
 
+import androidx.annotation.DrawableRes
+
 data class Producto(
     val id: Int = 0,
     val nombre: String = "",
     val precio: Double = 0.0,
     val disponible: Boolean = true,
     val descripcion: String = "",
-    val imagenUrl: String = "",
+    @DrawableRes val imagenRes: Int = 0, // 🔹 en vez de imagenUrl
     val stock: Int = 0,
     val categoria: String = "Ropa",
     val tallas: List<String> = listOf("S", "M", "L", "XL")
